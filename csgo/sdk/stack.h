f4e3e174e498ee1d04e5fd0cf36590d3e9eb0bd7
sdkabuser <45989733+sdkabuser@users.noreply.github.com>
2019-04-13 15:53:34 +1000

    update to stack.h

--- csgo/sdk/stack.h
+++ csgo/sdk/stack.h
@@ -1,32 +1,32 @@
 #pragma once
 
-class Stack : public Address
+class stack_t : public address_t
 {
 public:
 	// ctor.
-	__forceinline Stack() : Address{ *(uintptr_t *)((uintptr_t)_AddressOfReturnAddress() - sizeof(uintptr_t)) } { }
+	__forceinline stack_t() : address_t{ *(uintptr_t *)((uintptr_t)_AddressOfReturnAddress() - sizeof(uintptr_t)) } { }
 
 	// get return address.
-	__forceinline Address get_return_address() {
+	__forceinline address_t get_return_address() {
 		if (!m_ptr)
-			return Address{};
+			return address_t{};
 
 		return *(uintptr_t*) (m_ptr + sizeof(uintptr_t));
 	}
 
 	// go up stack frame.
-	__forceinline Stack& next() {
-		return *(Stack*) m_ptr;
+	__forceinline stack_t& next() {
+		return *(stack_t*) m_ptr;
 	}
 
 	// get local variable.
-	template< typename t = Address >
+	template< typename t = address_t >
 	__forceinline t get_local(size_t offset) {
 		return (t) (m_ptr - offset);
 	}
 
 	// get arg.
-	template< typename t = Address >
+	template< typename t = address_t >
 	__forceinline t get_arg(size_t offset) {
 		return (t) (m_ptr + offset);
 	}
